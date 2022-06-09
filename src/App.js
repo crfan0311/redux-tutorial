9c2566b62cd70b590ee7506646b47eaa3b47bcd6
shinystar0926 <shinystar0926@gmail.com>
2022-06-09 17:40:14 +0800

    update dashboard and posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import DashboardPage from './pages/DashboardPage'
+import PostsPage from './pages/PostsPage'
+
 function App() {
   return (
-    <div>
-      Hello, Redux
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<DashboardPage/>} />
+        <Route path='/posts' element={<PostsPage/>} />
+        <Route path="*" element={<Navigate to="/" replace />}/>
+      </Routes>
+    </Router>
   );
 }
 
